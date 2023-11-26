dcbe08e50af9982c5f47a529f835d1076fb9db4b
Toni G. <tty.gomez91@gmail.com>
2023-11-26 03:05:39 +0100

    get profiles list file, get metadata file for profile id, get profile file by id

--- src/Utils/HGitHub.ts
+++ src/Utils/HGitHub.ts
@@ -1,5 +1,5 @@
 export async function getLatestRelease() {
-    var myHeaders = new Headers();
+    const myHeaders = new Headers();
     myHeaders.append("Accept", "application/vnd.github+json");
     myHeaders.append("X-GitHub-Api-Version", "2022-11-28");
 
@@ -19,13 +19,53 @@ export async function getLatestRelease() {
 }
 
 export async function listProfiles() {
-    var myHeaders = new Headers();
+    const myHeaders = new Headers();
     myHeaders.append("Accept", "application/vnd.github+json");
     myHeaders.append("X-GitHub-Api-Version", "2022-11-28");
 
     let latestRelease = null
 
-    await fetch("https://api.github.com/repos/tonityg/RubyClientReleases/contents/profiles", {
+    await fetch("https://api.github.com/repos/tonityg/RubyClientReleases/contents/profiles.json", {
+        method: 'GET',
+        headers: myHeaders,
+        redirect: 'follow'
+    })
+        .then(async response => await response.json().then((data) => {
+            latestRelease = data
+        }))
+        .catch(error => console.log("Can't retrieve latest release on GitHub... Can't check for update."));
+
+    return latestRelease
+}
+
+export async function getMetadata(id: string) {
+    const myHeaders = new Headers();
+    myHeaders.append("Accept", "application/vnd.github+json");
+    myHeaders.append("X-GitHub-Api-Version", "2022-11-28");
+
+    let latestRelease = null
+
+    await fetch( `https://api.github.com/repos/tonityg/RubyClientReleases/contents/profile/${id}.json`, {
+        method: 'GET',
+        headers: myHeaders,
+        redirect: 'follow'
+    })
+        .then(async response => await response.json().then((data) => {
+            latestRelease = data
+        }))
+        .catch(error => console.log("Can't retrieve latest release on GitHub... Can't check for update."));
+
+    return latestRelease
+}
+
+export async function getProfileData(id: string) {
+    const myHeaders = new Headers();
+    myHeaders.append("Accept", "application/vnd.github+json");
+    myHeaders.append("X-GitHub-Api-Version", "2022-11-28");
+
+    let latestRelease = null
+
+    await fetch( `https://api.github.com/repos/tonityg/RubyClientReleases/contents/metadata/${id}.json`, {
         method: 'GET',
         headers: myHeaders,
         redirect: 'follow'
