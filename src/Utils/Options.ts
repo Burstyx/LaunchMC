d7846e7f419397e71edc218ad51c9f9458534419
Toni G. <tty.gomez91@gmail.com>
2023-12-29 23:15:30 +0100

    created option manager methods to get and set setting property

---
+++ src/Utils/Options.ts
@@ -0,0 +1,40 @@
+import {readFile} from "fs-extra";
+import path from "path";
+import {gamePath} from "./const";
+import {existsSync} from "fs";
+
+async function setSetting(property: string, value: string) {
+    return new Promise<void>(async (resolve, reject) => {
+        const settingsPath = path.join(gamePath, "settings.json")
+        let data: any = {}
+
+        if(existsSync(settingsPath)) {
+            await readFile(path.join(gamePath, "settings.json"), "utf8").then((val) => {
+                data = JSON.parse(val)
+            }).catch((err) => reject(err))
+        }
+
+        data[property] = value
+
+        resolve()
+    })
+}
+
+async function getSetting(property: string) {
+    return new Promise<void>(async (resolve, reject) => {
+        const settingsPath = path.join(gamePath, "settings.json")
+
+        if(existsSync(settingsPath)) {
+            await readFile(path.join(gamePath, "settings.json"), "utf8").then((val) => {
+                const data = JSON.parse(val)
+                if(data.hasOwnProperty(property)) {
+                    resolve(data[property])
+                }else {
+                    reject()
+                }
+            }).catch((err) => reject(err))
+        } else {
+            reject()
+        }
+    })
+}
